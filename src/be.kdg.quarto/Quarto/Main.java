b51fb90cd97255e337ee0ff932bfded1c745b100
boyan <106580794+bobyTopG@users.noreply.github.com>
2025-02-17 11:27:48 +0100

    POC

---
+++ src/be.kdg.quarto/Quarto/Main.java
@@ -0,0 +1,23 @@
+package src.be.kdg.quarto.Quarto;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import src.be.kdg.quarto.model.Game;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Game model = new Game();
+       // StartView view = new StartView();
+       //primaryStage.setScene(new Scene(view));
+       // new StartPresenter(model, view);
+        primaryStage.setHeight(400);
+        primaryStage.setWidth(850);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
